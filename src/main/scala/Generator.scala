bdf82ae8de89bf98d116957883da9109a5fde7e2
Paul Wrubel <pjwrubel@mtu.edu>
2018-03-18 15:13:01 -0400

    cleaned up for beginning real work

---
+++ src/main/scala/Generator.scala
@@ -0,0 +1,14 @@
+import javafx.scene.image.{Image, WritableImage}
+
+object Generator {
+
+    val width = 500
+    val height = 500
+
+    def main(args: Array[String]): Unit = {
+
+        val image = new WritableImage(width, height)
+
+    }
+
+}
--- src/ws.sc
+++
@@ -1,7 +0,0 @@
-object test {
-
-  def main(args: Array[String]): Unit = {
-    println("Hello, World!")
-  }
-
-}
