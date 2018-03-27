90894c965da1cc5fe0af27c284c98a089bd2722f
Paul Wrubel <pjwrubel@mtu.edu>
2018-03-27 13:01:52 -0400

    changed vals to vars for future GUI customization, added some more parameters to use in future developments

--- src/main/scala/me/paul/artgenerators/Parameters.scala
+++ src/main/scala/me/paul/artgenerators/Parameters.scala
@@ -2,43 +2,47 @@ package me.paul.artgenerators
 
 object Parameters {
 
-    val Debug: Boolean = true
-    val Version: String = "v0.3-alpha"
+    var Debug: Boolean = true
+    var Version: String = "v0.4g-alpha"
 
-    val ImageCount: Int = 1
-    val OpenFile: Boolean = true
+    var ImageCount: Int = 1
+    var OpenFile: Boolean = false
 
     // HD                 : 1280  x 720
     // Full HD            : 1920  × 1080
     // True 4K            : 4096  × 2160
     // True 8K            : 8192  × 4320
     // Max Possible (32K) : 30720 x 17280
-    val Width: Int  = 1920
-    val Height: Int = 1080
+    var Width: Int  = 1920
+    var Height: Int = 1080
 
-    val Filename: String   = f"$Version-${Width}x$Height"
-    val Filepath: String   = f"./out/images/$Version/${Width}x$Height/"
-    val FileFormat: String = "png"
+    var Filename: String   = f"$Version-${Width}x$Height"
+    var Filepath: String   = f"./out/images/$Version/${Width}x$Height/"
+    var FileFormat: String = "png"
 
-    val SeedCount: Int = 1
+    var SeedCount: Int = 1
 
-    val HueVariation: Double        = 5
-    val SaturationVariation: Double = 0.02
-    val BrightnessVariation: Double = 0.02
+    var HueVariation: Double        = 1
+    var SaturationVariation: Double = 0.02
+    var BrightnessVariation: Double = 0.02
 
-    val HueBounds: (Double, Double)        = (90, 30)
-    val SaturationBounds: (Double, Double) = (0.5, 1)
-    val BrightnessBounds: (Double, Double) = (0.5, 1)
+    var HueVariationDelta: Double        = 0
+    var SaturationVariationDelta: Double = 0
+    var BrightnessVariationDelta: Double = 0
 
-    val NorthSpreadChance: Double = 0.25
-    val EastSpreadChance: Double  = 0.75
-    val SouthSpreadChance: Double = 0.25
-    val WestSpreadChance: Double  = 0.75
+    var HueBounds: (Double, Double)        = (60, 30)
+    var SaturationBounds: (Double, Double) = (0.5, 1)
+    var BrightnessBounds: (Double, Double) = (0.5, 1)
+
+    var NorthSpreadChance: Double = 0.0
+    var EastSpreadChance: Double  = 0.5
+    var SouthSpreadChance: Double = 0.0
+    var WestSpreadChance: Double  = 0.5
 
     // unused for now
-    val NorthSpreadChanceDelta: Double = -0.01
-    val EastSpreadChanceDelta: Double  = 0.01
-    val SouthSpreadChanceDelta: Double = -0.01
-    val WestSpreadChanceDelta: Double  = 0.01
+    var NorthSpreadChanceDelta: Double = 0.0005
+    var EastSpreadChanceDelta: Double  = 0.0005
+    var SouthSpreadChanceDelta: Double = 0.0005
+    var WestSpreadChanceDelta: Double  = 0.0005
 
 }
